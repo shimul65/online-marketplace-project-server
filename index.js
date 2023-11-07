76a0c617452c58becc6f865a90196ae225758b4d
Shimul <shimulmohammad420@gmail.com>
2023-11-08 01:11:02 +0600

    conenct to mongodb and dotenv file create

--- index.js
+++ index.js
@@ -8,5 +8,79 @@ const port = process.env.PORT || 5055;
 app.use(cors());
 app.use(express.json());
 
-app.get('/', (req, res) => { res.send('Simple coffees server is running') })
-app.listen(port, () => { console.log(`Simple coffees server is running on port ${port}`) })
+
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.n45ephu.mongodb.net/?retryWrites=true&w=majority`;
+
+// Create a MongoClient with a MongoClientOptions object to set the Stable API version
+const client = new MongoClient(uri, {
+    serverApi: {
+        version: ServerApiVersion.v1,
+        strict: true,
+        deprecationErrors: true,
+    }
+});
+
+async function run() {
+    try {
+        // await client.connect();
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+        await client.db("admin").command({ ping: 1 });
+        console.log("Pinged your deployment. You successfully connected to MongoDB!");
+    } finally {
+        // await client.close();
+    }
+}
+run().catch(console.dir);
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+app.get('/', (req, res) => {
+    res.send('online marketplace server is running')
+})
+
+app.listen(port, () => {
+    console.log(`Simple online marketplace is running on port ${port}`)
+})
